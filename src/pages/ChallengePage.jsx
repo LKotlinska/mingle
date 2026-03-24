313aab3916c96fd286c9899b0c4b666c99e59077
Daniella Saadon <saadondaniella@gmail.com>
2026-03-24 11:57:50 +0100

    Add navigation bar and routes to diffrent pages

---
+++ src/pages/ChallengePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ChallengePage.css";
+
+export default function ChallengePage() {
+  return (
+    <main className="challenge-page">
+      <h1>UTMANINGAR</h1>
+      <p>Här kommer listan med utmaningar.</p>
+    </main>
+  );
+}
---
+++ src/pages/Match.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Match.css";
+
+export default function Match() {
+  return (
+    <main className="match-page">
+      <h1>MATCH</h1>
+      <p>Här kommer matchningsflödet för studenter och företag.</p>
+    </main>
+  );
+}
