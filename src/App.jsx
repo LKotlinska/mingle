8f4c64044e6b48f935c76c92d7f80f84394cd38b
Daniella Saadon <saadondaniella@gmail.com>
2026-03-31 10:33:16 +0200

    Change filename in route

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import ChallengePage from "./pages/ChallengePage.jsx";
 import CompanyPage from "./pages/CompanyPage.jsx";
 import CompanyList from "./pages/CompanyList.jsx";
 import StudentPage from "./pages/StudentPage.jsx";
-import RegisterPage from "./pages/RegisterPage.jsx";
 import RegistrationPage from "./pages/RegistrationPage.jsx";
 
 function AppContent() {
@@ -32,7 +31,7 @@ function AppContent() {
         <Route path="/utmaningar" element={<ChallengePage />} />
         <Route path="/company" element={<CompanyPage />} />
         <Route path="/company-list" element={<CompanyList />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
         <Route path="/foretag" element={<Navigate to="/company" replace />} />
       </Routes>
       <Footer />
