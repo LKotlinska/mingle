2838fcf0ee76f9fce6cfd84c98d2c213fa2ffc97
Your Name <laura.kotlinska.96@gmail.com>
2026-04-01 14:33:19 +0200

    Adjust error message and run DB connection on startup

--- api/register.js
+++ api/register.js
@@ -1,23 +1,15 @@
 import express from "express";
-import mongoose from "mongoose";
-import "dotenv/config";
 import Company from "../models/company.js";
 
 const router = express.Router();
 
-const connectDB = async () => {
-  await mongoose.connect(process.env.DB_URI);
-};
-
 router.post("/", async (req, res) => {
-  await connectDB();
-
   const { code, name, employment, skills, traits } = req.body;
 
   const company = await Company.findOne({ code: code });
 
   if (!company) {
-    return res.status(404).json({ error: "No company matching this code" });
+    return res.json({ error: "No company matching this code" });
   }
   company.name = name;
   company.employment = employment;
@@ -28,12 +20,11 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/:code", async (req, res) => {
-  await connectDB();
   const { code } = req.params;
   const company = await Company.findOne({ code: code });
-  console.log(company);
 
-  if (!company) return res.status(404).json({ error: "Not found" });
+  if (!company)
+    return res.status(404).json({ error: "No company matching this code" });
 
   res.status(200).json(company);
 });
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 import router from "./api/register.js";
 
 const app = express();
@@ -8,4 +9,6 @@ app.use(express.json());
 
 app.use("/api/register", router);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+mongoose.connect(process.env.DB_URI).then(() => {
+    app.listen(3000, () => console.log("Server running on port 3000"));
+});
