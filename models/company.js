672dc72eb66e7635af240fec8517415a601a70cd
Your Name <laura.kotlinska.96@gmail.com>
2026-04-01 14:38:17 +0200

    No dots at the end of sentence

--- models/company.js
+++ models/company.js
@@ -21,19 +21,19 @@ const companySchema = new mongoose.Schema({
 function validateCompany(company) {
   const schema = Joi.object({
     name: Joi.string().trim().min(1).max(25).required().messages({
-      "string.base": "Namnet måste vara text.",
-      "string.empty": "Namnet får inte vara tomt.",
-      "string.min": "Namnet är för kort.",
-      "string.max": "Namnet får max vara 25 tecken.",
-      "any.required": "Namnet krävs.",
+      "string.base": "Namnet måste vara text",
+      "string.empty": "Namnet får inte vara tomt",
+      "string.min": "Namnet är för kort",
+      "string.max": "Namnet får max vara 25 tecken",
+      "any.required": "Namnet krävs",
     }),
     employment: Joi.array()
       .items(Joi.string().trim().min(1))
       .min(1)
       .required()
       .messages({
-        "array.min": "Minst en anställningstyp krävs.",
-        "any.required": "Anställningstyp krävs.",
+        "array.min": "Minst en anställningstyp krävs",
+        "any.required": "Anställningstyp krävs",
       }),
     traits: Joi.array().items(Joi.string().trim().min(1)),
     skills: Joi.array().items(Joi.string().trim().min(1)),
