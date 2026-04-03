731fcdbb01123b8258376208328e0695f3b54715
Your Name <laura.kotlinska.96@gmail.com>
2026-04-03 08:37:14 +0200

    Hides symbols for screen readers

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,7 +2,7 @@ import './Alert.css'
 export default function Alert({alert}) {
     return(
         <p className="alert">
-            <span className="material-symbols-outlined alertSymbol">check_circle</span>
+            <span className="material-symbols-outlined alertSymbol" aria-hidden="true">check_circle</span>
             {alert}
         </p>
     )
--- src/components/forms/InlineError.jsx
+++ src/components/forms/InlineError.jsx
@@ -3,7 +3,7 @@ import './InlineError.css'
 export default function InlineError({error}) {
     return(
         <p className="fieldError">
-            <span className="material-symbols-outlined errorSymbol">error</span>
+            <span className="material-symbols-outlined errorSymbol" aria-hidden="true">error</span>
             {error}
         </p>
     )
