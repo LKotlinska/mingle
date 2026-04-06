f39c75d8d68840b0f0e2639200800ccf02b392b5
Your Name <laura.kotlinska.96@gmail.com>
2026-04-06 15:58:29 +0200

    Adds form for the matching function

--- src/pages/Match.jsx
+++ src/pages/Match.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Match.css";
 import BackLink from "../components/BackLink";
+import MatchingForm from "../components/MatchingForm";
 
 export default function Match() {
   return (
-    <main className="match-page">
-      <BackLink to="/student" />
-      <h1 className="title-pill">MATCH</h1>
-      <p>Här kommer matchningsflödet för studenter och företag.</p>
-    </main>
+      <main className="registrationPage">
+        <div className="registrationSection">
+          <h1 className="formHeading">Hitta din match</h1>
+           <MatchingForm/>
+        </div>
+      </main>
   );
 }
