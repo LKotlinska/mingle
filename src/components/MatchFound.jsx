b19c6d8398232aa25cda1a65a4585febd97f252a
Daniella Saadon <saadondaniella@gmail.com>
2026-04-09 15:10:47 +0200

    Add a button on match page

--- src/components/MatchFound.jsx
+++ src/components/MatchFound.jsx
@@ -4,59 +4,73 @@ import "../pages/StudentList.css";
 import "./MatchFound.css";
 
 export default function MatchFound({ companies, onMatch }) {
-    const [best, ...rest] = companies;
+  const [best, ...rest] = companies;
 
-    return(
-        <section className="matchSection">
-            <h1>
-                Det är en <span className="uppercase">match</span>!
-            </h1>
-            <div className="matchContainer">
-                <div className="cards">
-                    <img src="/doodlez/doodle-left.png" alt="" className="doodle doodle--topleft" />
-                    <img src="/doodlez/doodle-right.png" alt="" className="doodle doodle--bottomright" />
-                    <img src="/doodlez/stars.png" alt="" className="doodle doodle--stars-left" />
-                    <img src="/doodlez/stars.png" alt="" className="doodle doodle--stars-right" />
-                    <MatchCard
-                        fileName={"foretag"}
-                        cardFor={"företag"}
-                    />
-                    <MatchCard
-                        fileName={"kandidat"}
-                        cardFor={"kandidat"}
-                    />
-                </div>
-                <h2 className="bestMatch">
-                    {best.company.name}
-                </h2>
-            </div>
+  return (
+    <section className="matchSection">
+      <h1>
+        Det är en <span className="uppercase">match</span>!
+      </h1>
+      <div className="matchContainer">
+        <div className="cards">
+          <img
+            src="/doodlez/doodle-left.png"
+            alt=""
+            className="doodle doodle--topleft"
+          />
+          <img
+            src="/doodlez/doodle-right.png"
+            alt=""
+            className="doodle doodle--bottomright"
+          />
+          <img
+            src="/doodlez/stars.png"
+            alt=""
+            className="doodle doodle--stars-left"
+          />
+          <img
+            src="/doodlez/stars.png"
+            alt=""
+            className="doodle doodle--stars-right"
+          />
+          <MatchCard fileName={"foretag"} cardFor={"företag"} />
+          <MatchCard fileName={"kandidat"} cardFor={"kandidat"} />
+        </div>
+        <h2 className="bestMatch">{best.company.name}</h2>
+      </div>
 
-              
-            <div className="student-list-page">
-                <div className="student-grid">
-                <p className="intro">More matches</p>
-                    {rest.map(({ company }) => (
-                        <div key={company._id} className="student-card">
-                            <div className="student-card-blob" />
-                            <div className="student-card-content company-card-content">
-                                <div className="student-card-text">
-                                    <h3 className="student-name">{company.name}</h3>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+      <div className="student-list-page">
+        <div className="student-grid">
+          <p className="intro">More matches</p>
+          {rest.map(({ company }) => (
+            <div key={company._id} className="student-card">
+              <div className="student-card-blob" />
+              <div className="student-card-content company-card-content">
+                <div className="student-card-text">
+                  <h3 className="student-name">{company.name}</h3>
                 </div>
+              </div>
             </div>
-            <div className="matchBtnContainer">
-            <Link to="/match">
-                <button className='btnSubmit btnUndo' onClick={() => onMatch(null)} type="button">
-                    Ny match
-                    <span class="material-symbols-outlined">
-                    autorenew
-                    </span>
-                </button>
-            </Link>
-            </div>
-        </section>
-    )
+          ))}
+        </div>
+      </div>
+      <div className="matchBtnContainer">
+        <Link to="/match">
+          <button
+            className="btnSubmit btnUndo"
+            onClick={() => onMatch(null)}
+            type="button"
+          >
+            Ny match
+            <span className="material-symbols-outlined">autorenew</span>
+          </button>
+        </Link>
+        <Link to="/utmaningar">
+          <button className="btnSubmit" type="button">
+            Utmana dig
+          </button>
+        </Link>
+      </div>
+    </section>
+  );
 }
