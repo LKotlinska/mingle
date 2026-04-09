58916d424bae2ad69cb9a236c9f549f059e7052d
Your Name <laura.kotlinska.96@gmail.com>
2026-04-09 15:35:45 +0200

    Adjust size of matching cards and aminations

--- src/components/MatchFound.jsx
+++ src/components/MatchFound.jsx
@@ -16,7 +16,7 @@ export default function MatchFound({ companies, onMatch }) {
                     <img src="/doodlez/doodle-left.png" alt="" className="doodle doodle--topleft" />
                     <img src="/doodlez/doodle-right.png" alt="" className="doodle doodle--bottomright" />
                     <img src="/doodlez/stars.png" alt="" className="doodle doodle--stars-left" />
-                    <img src="/doodlez/stars.png" alt="" className="doodle doodle--stars-right" />
+                    <img src="/doodlez/stars-right.png" alt="" className="doodle doodle--stars-right" />
                     <MatchCard
                         fileName={"foretag"}
                         cardFor={"företag"}
@@ -31,31 +31,32 @@ export default function MatchFound({ companies, onMatch }) {
                 </h2>
             </div>
 
-              
-            <div className="student-list-page">
-                <div className="student-grid">
-                <p className="intro">More matches</p>
-                    {rest.map(({ company }) => (
-                        <div key={company._id} className="student-card">
-                            <div className="student-card-blob" />
-                            <div className="student-card-content company-card-content">
-                                <div className="student-card-text">
-                                    <h3 className="student-name">{company.name}</h3>
+            <div className="list-content">
+                <div className="student-list-page">
+                    <div className="student-grid">
+                    <p className="intro">More matches</p>
+                        {rest.map(({ company }) => (
+                            <div key={company._id} className="student-card">
+                                <div className="student-card-blob" />
+                                <div className="student-card-content company-card-content">
+                                    <div className="student-card-text">
+                                        <h3 className="student-name">{company.name}</h3>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
-            </div>
-            <div className="matchBtnContainer">
-            <Link to="/match">
-                <button className='btnSubmit btnUndo' onClick={() => onMatch(null)} type="button">
-                    Ny match
-                    <span class="material-symbols-outlined">
-                    autorenew
-                    </span>
-                </button>
-            </Link>
+                <div className="matchBtnContainer">
+                    <Link to="/match">
+                        <button className='btnSubmit btnUndo' onClick={() => onMatch(null)} type="button">
+                            Ny match
+                            <span class="material-symbols-outlined">
+                            autorenew
+                            </span>
+                        </button>
+                    </Link>
+                </div>
             </div>
         </section>
     )
